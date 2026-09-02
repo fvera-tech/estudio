c6953f98fda209be3b150a2480a2958a8b338595
Fabian Vera <fvera.dev@gmail.com>
2026-09-02 16:24:09 -0400

    First commit

---
+++ ejercicio4-async-await.js
@@ -0,0 +1,8 @@
+// EJERCICIO DONDE SE PRACTICARÁ ASYNC/AWAIT MEDIANTE UNA API GRATUITA DE CLIMA. LA FUNCIÓN DEBE RECIBIR UNA CIUDAD Y ENTREGAR LA TEMPERATURA DE LA MISMA.
+
+function climaActual(ciudad) {
+    if (typeof ciudad !== "string") return "Ingrese una ciudad válida.";
+
+
+
+}
