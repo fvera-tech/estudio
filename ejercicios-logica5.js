59e79b60999bf1df72a471bad9229c47448fcec2
Fabian Vera <fvera.dev@gmail.com>
2026-09-15 16:59:26 -0300

    Ejercicios de lógica para seguir trabajando con JavaScript y mejorar el pensamiento abstracto.

---
+++ ejercicios-logica5.js
@@ -0,0 +1,32 @@
+function capitalizarTitulo(titulo) {
+
+    if (typeof titulo !== "string") return "Ingresa un título válido.";
+    if (titulo === "") return "Ingresa un título válido.";
+
+    const palabrasSeparadas = titulo.split(" ");
+    const palabrasCapitalizadas = palabrasSeparadas.map((palabra) => {
+        const inicialAMayuscula = palabra.charAt(0).toUpperCase();
+        const resto = palabra.slice(1, palabra.length);
+        return inicialAMayuscula + resto
+    })
+
+    return palabrasCapitalizadas.join(" ");
+
+}
+
+function segundosAMinutos(num) {
+    if (typeof num !== "number") return "Ingrese un número válido";
+    if (num < 0) return "Ingrese un tiempo válido";
+
+    const minutos = Math.floor(num / 60);
+    const segundos = num % 60;
+
+    const minutosString = String(minutos)
+    const segundosString = String(segundos)
+
+
+    return `${minutosString.padStart(2, "0")}:${segundosString}`
+
+}
+
+console.log(segundosAMinutos(879))
